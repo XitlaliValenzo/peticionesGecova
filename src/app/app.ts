bf9d1f20a27055f70662863234981c2b1ca57a15
Xitlali Valenzo <ma.xitlalivs@gmail.com>
2025-07-18 20:36:50 -0600

    se quitan los import que no se utilizan en app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TablaComponent } from './tabla-component/tabla-component'
 import { NavbarComponent } from "./navbar-component/navbar-component";
 import { BannerComponent } from "./banner-component/banner-component";
-import { ButtonComponent } from "./button-component/button-component";
-import { FiltrosComponent } from "./filtros-component/filtros-component";
 import { ContainerTableComponent } from "./container-table-component/container-table-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TablaComponent, NavbarComponent, BannerComponent, ButtonComponent, FiltrosComponent, ContainerTableComponent],
+  imports: [RouterOutlet, NavbarComponent, BannerComponent, ContainerTableComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
