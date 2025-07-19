a265e68957302d1f3ff1e44ff05d425f5dbf58d3
Xitlali Valenzo <ma.xitlalivs@gmail.com>
2025-07-18 20:34:02 -0600

    add se agrega el tooltip del boton de nueva peticion

--- src/app/button-component/button-component.ts
+++ src/app/button-component/button-component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-button-component',
-  imports: [MatIconModule, MatDividerModule,MatButtonModule],
+  imports: [MatIconModule, MatDividerModule,MatButtonModule,MatTooltipModule ],
   templateUrl: './button-component.html',
   styleUrl: './button-component.css'
 })
 export class ButtonComponent {
-
+  tooltipText = 'Nueva Petición - Agregar registros';
 }
